02bf87873f8533c3bbe4c811f0d57478dc76fc46
Dharmesh Patel <dharmesh020@hotmail.com>
2017-10-19 02:03:03 +1100

    Testing

--- BTreeEmptyContainerFinderProject/Program.cs
+++ BTreeEmptyContainerFinderProject/Program.cs
@@ -12,36 +12,50 @@ namespace BTreeEmptyContainerFinderProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Binary Tree Traversal System");
-            // Console.ReadLine();
-           var bTreeCreationAndTraversalLogicObj= FactoryOfObjects<IGateNodeTree>.Create();
-
-               //  GateNodeTree bTreeCreationAndTraversalLogicObj = new GateNodeTree();
-            Console.WriteLine("Please enter depth of the binary tree:");
-            try
+           
+            bool trycount=true;
+            while (trycount)
             {
-                var depthOfBinaryTree = Convert.ToInt32(Console.ReadLine());
-                //Initialization of Binary Tree of GateNodes
-                bTreeCreationAndTraversalLogicObj.InitializeGateNodeTree(depthOfBinaryTree);
-                Console.WriteLine("No of balls to pass into binary tree:");
-                var noOfBallsToPassIntoBinaryTree = Convert.ToInt32(Console.ReadLine());
-                //Traversal Of GateNodes Branches with No oF balls
-                var emptyContainers = bTreeCreationAndTraversalLogicObj.GateNodeTreeTraversalForAllBallsToFindEmptyContainer(noOfBallsToPassIntoBinaryTree);
-
-                Console.WriteLine("------------------------------------------------------------------------------------------------------------------");
-               Console.WriteLine("Total Empty Containers:{0}", emptyContainers.Count());
-                Console.WriteLine("Empty Containers( starting first container from left.):");
-                foreach (var emptyContainer in emptyContainers)
+                try
                 {
+                   var bTreeCreationAndTraversalLogicObj = FactoryOfObjects<IGateNodeTree>.Create();
 
-                    Console.WriteLine( "Node ID : {0}, Container ID {1}",emptyContainer.NodeId, emptyContainer.containerId);
+                    Console.WriteLine("Welcome to Binary Tree Traversal System");
+                    Console.WriteLine("Please enter depth of the binary tree ( Less than or equal to 10):");
+                    var depthOfBinaryTree = Convert.ToInt32(Console.ReadLine());
+                    if (depthOfBinaryTree > 10)
+                        continue;
+
+
+                    //Initialization of Binary Tree of GateNodes
+                    bTreeCreationAndTraversalLogicObj.InitializeGateNodeTree(depthOfBinaryTree);
+                    Console.WriteLine("No of balls to pass into binary tree:");
+                    var noOfBallsToPassIntoBinaryTree = Convert.ToInt32(Console.ReadLine());
+                    //Traversal Of GateNodes Branches with No oF balls
+                    var emptyContainers = bTreeCreationAndTraversalLogicObj.GateNodeTreeTraversalForAllBallsToFindEmptyContainer(noOfBallsToPassIntoBinaryTree);
+
+                    Console.WriteLine("------------------------------------------------------------------------------------------------------------------");
+                    Console.WriteLine("Total Empty Containers:{0}", emptyContainers.Count());
+                    Console.WriteLine("Empty Containers( starting first container from left.):");
+                    foreach (var emptyContainer in emptyContainers)
+                    {
+
+                        Console.WriteLine("Node ID : {0}, Container ID {1}", emptyContainer.NodeId, emptyContainer.containerId);
+                    }
+                    Console.WriteLine("------------------------------------------------------------------------------------------------------------------");
+                    Console.WriteLine("Press 0 to exit or any other key to continue.");
+                    var res= Console.ReadLine();
+                    if (res == "0") trycount = false;
+                   
+                   
                 }
-                Console.ReadLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid input");
+                    trycount = true;
+                    continue;
+                }
+
             }
            
 
